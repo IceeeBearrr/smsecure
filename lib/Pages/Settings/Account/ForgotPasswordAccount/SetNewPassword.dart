bb21841d25b0b46f477aff2b56831908598327fc
Janice <janiceeminnzly@gmail.com>
2024-11-16 04:55:57 +0800

    yay

--- lib/Pages/Settings/Account/ForgotPasswordAccount/SetNewPassword.dart
+++ lib/Pages/Settings/Account/ForgotPasswordAccount/SetNewPassword.dart
@@ -1,6 +1,5 @@
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
-import 'package:smsecure/Pages/Home/HomePage.dart';
 
 class SetNewPassword extends StatefulWidget {
   final String phone;
